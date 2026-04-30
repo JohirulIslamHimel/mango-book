7798887cafa8bfa1bc95edd6dad8a655d98c1daa
Johirul Islam Himel <himel1199@gmail.com>
2026-05-01 01:34:26 +0600

    mongodb setup

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-52 gap-4"
+              className="menu menu-sm dropdown-content mt-3 z-1 p-4 shadow bg-base-100 rounded-box w-52 gap-4"
             >
               {navLinks}
             </ul>
