b6c161ed7cf6e46f6b8eadf487197840242444cc
Johirul Islam Himel <himel1199@gmail.com>
2026-05-03 14:16:13 +0600

    PRIVATE ROUTE implemented

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  const sessionCookie =
+    request.cookies.get("better-auth.session_token") ||
+    request.cookies.get("__secure-better-auth.session_token");
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/my-profile/:path*", "/all-books/:path*"],
+};
